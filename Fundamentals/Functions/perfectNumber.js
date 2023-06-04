4f88b6d56e7228bef1c4d22a9c8c41cc76622df7
Borislav Andreev Georgiev <bo0bkat7a@hotmail.com>
2023-06-04 21:12:20 +0300

    perfectNumber

---
+++ Fundamentals/Functions/perfectNumber.js
@@ -0,0 +1,17 @@
+function perfectNumber(num) {
+
+    let sum = 0;
+
+    for(let i = 1; i <= num / 2; i++) {
+        if(num % i === 0) {
+            sum += i;
+        }
+    }
+    if(sum === num && num !== 1){
+        console.log("We have a perfect number!");
+    }else{
+        console.log("It's not so perfect.");
+    }
+}
+
+perfectNumber(6)
