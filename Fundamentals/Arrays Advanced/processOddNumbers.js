1551d0beffbd7166ffae1ed8efd1cc01373c5e0b
Borislav Andreev Georgiev <bo0bkat7a@hotmail.com>
2023-06-10 15:19:49 +0300

    one row slove

--- Fundamentals/Arrays Advanced/processOddNumbers.js
+++ Fundamentals/Arrays Advanced/processOddNumbers.js
@@ -1,10 +1,11 @@
 function processOddNumbers(array) {
 
-    let arrOddNum = array.filter((el, i) => i % 2 !== 0);
-    let sumArrayNum = arrOddNum.map(a => a * 2);
-    let arrRevercce = sumArrayNum.reverse()
-    console.log(arrRevercce.join(" "));
+//    let arrOddNum = array.filter((el, i) => i % 2 !== 0);
+//    let sumArrayNum = arrOddNum.map(a => a * 2);
+//    let arrRevercce = sumArrayNum.reverse()
+//    console.log(arrRevercce.join(" "));
 
+ return array.filter((el, index) => index % 2 !== 0).map(x => x * 2).reverse().join(" ")
 }
 
-processOddNumbers([3, 0, 10, 4, 7, 3])
+console.log(processOddNumbers([3, 0, 10, 4, 7, 3]));
